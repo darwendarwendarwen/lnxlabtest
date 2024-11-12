96fdbbb2c6ec5db1c564062534b2a8e32086ef93
darwendarwendarwen <darwin.dwin.luk@gmail.com>
2024-11-12 14:14:08 -0500

    Removed test

--- test/.myapp.cmd
+++
@@ -1,1 +0,0 @@
-cmd_myapp := /home/darwin/x-tools/aarch64-seneca-linux-gnu/bin/aarch64-seneca-linux-gnu-gcc  -o myapp -Wl,--start-group lib/lib.a student_information_darwin/lib.a main/built-in.o  -Wl,--end-group
--- test/student_information_darwin/LNX500_Test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-char* announce_student(void);
-
-char* announce_student(){
-	return "Darwin Luk ID:150319226";
-}
-
-
